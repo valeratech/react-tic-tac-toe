0180a7335927f7de3da639ed432c90898af968c7
Ryan Valera <valeraryan@gmail.com>
2023-11-08 11:17:05 -0800

    Add reusable Player.jsx component to create a player info/interactive box for the UI.

---
+++ src/components/Player.jsx
@@ -0,0 +1,13 @@
+function Player({name, symbol}) {
+    return (
+        <li>
+            <span className="player">
+              <span className="player-name">{name}</span>
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button>Edit</button>
+        </li>
+    );
+}
+
+export default Player;
