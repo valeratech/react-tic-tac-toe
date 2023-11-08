7a17d19e9bfbb2c61f7bb7b4ef52304026500543
Ryan Valera <valeraryan@gmail.com>
2023-11-08 12:09:14 -0800

    Modify Player.jsx module to include editing for the Player-container when 'edit' button is selected.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 function Player({name, symbol}) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    function editClickHandler(e) {
+        setIsEditing(prev => !prev);
+        console.log(isEditing);
+    }
+
+    let playerNameSpan;
+    isEditing ? playerNameSpan = <input type="text" placeholder="Enter Name" required />
+        : playerNameSpan = <span className="player-name">{name}</span>;
+
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {playerNameSpan}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={editClickHandler}>Edit</button>
         </li>
     );
 }
